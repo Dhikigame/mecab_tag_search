91d2d6ca4c9c64dcd33184ef9962def7bca3b733
daiki <dadada7772000@yahoo.co.jp>
2018-10-14 15:03:47 +0900

    sub_tag regist

--- sub_tag_search.php
+++ sub_tag_search.php
@@ -22,7 +22,7 @@ function sub_tag_search($word, $sports_num){
 			$tag[$j] = "野球";
 			$j++;
 		}
-		if($word == "NPB" || $word == "プロ野球" || $word == "12球団" || $word == "オールスター" || $word == "ファン"){
+		if($word == "NPB" || $word == "プロ野球" || $word == "12球団" || $word == "オールスター" || $word == "ファン" || $word == "日本シリーズ" || $word == "日シリ" || $word == "CS"){
 			$tag[$j] = "プロ野球";
 			$j++;
 		}
@@ -192,7 +192,7 @@ function sub_tag_search($word, $sports_num){
 			$tag[$j] = "Jリーグ";
 			$j++;
 		}
-		if($word == "海外サッカー" || $word == "W杯" || $word == "Ｗ杯" || $word == "ワールドカップ" || $word == "ACL" || $word == "AFCチャンピオンズリーグ" || $word == "アジア" || $word == "アジアカップ" || $word == "日本代表" || $word == "Ｕ－１６" || $word == "Ｕ１６" || $word == "U-16" || $word == "U16" || $word == "Ｕ-２０" || $word == "U-20" || $word == "U20" || $word == "Ｕ２０"  || $word == "なでしこ" || $word == "アジア選手権"){
+		if($word == "海外サッカー" || $word == "W杯" || $word == "Ｗ杯" || $word == "ワールドカップ" || $word == "ACL" || $word == "AFCチャンピオンズリーグ" || $word == "アジア" || $word == "アジアカップ" || $word == "日本代表" || $word == "Ｕ－１６" || $word == "Ｕ１６" || $word == "U-16" || $word == "U16" || $word == "Ｕ-２０" || $word == "U-20" || $word == "U20" || $word == "Ｕ２０"  || $word == "なでしこ" || $word == "アジア選手権"  || $word == "リーガ" || $word == "代表"  || $word == "ネーションズリーグ"){
 			$tag[$j] = "海外サッカー";
 			$j++;
 		}
